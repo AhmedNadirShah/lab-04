545f4e07704cf28201629252125a0a75e6dec0dc
AhmedNadirShah <ahmednadirshah99@gmail.com>
2024-09-14 00:34:45 +0500

    Add files via upload

---
+++ Fast/question 5.cpp
@@ -0,0 +1,84 @@
+#include<stdio.h>
+int main()
+{int time;
+printf("Enter the time of the day\n");
+scanf("%d",&time);
+switch(time)
+{case 1:
+printf("Good Night",time=0);
+break;
+case 2:
+printf("Good Night",time=1);
+break;
+case 3:
+printf("Good Night",time=2);
+break;
+case 4:
+printf("Good Night",time=3);
+break;
+case 5:
+printf("Good Night",time=4);
+break;
+case 6:
+printf("Good Night",time=5);
+break;
+case 7:
+printf("Good Morning",time=6);
+break;
+case 8:
+printf("Good Morning",time=7);
+break;
+case 9:
+printf("Good Morning",time=8);
+break;
+case 10:
+printf("Good Morning",time=9);
+break;
+case 11:
+printf("Good Morning",time=10);
+break;
+case 12:
+printf("Good Morning",time=11);
+break;
+case 13:
+printf("Good Afternoon",time=12);
+break;
+case 14:
+printf("Good Afternoon",time=13);
+break;
+case 15:
+printf("Good Afternoon",time=14);
+break;
+case 16:
+printf("Good Afternoon",time=15);
+break;
+case 17:
+printf("Good Afternoon",time=16);
+break;
+case 18:
+printf("Good Afternoon",time=17);
+break;
+case 19:
+printf("Good Evening",time=18);
+break;
+case 20:
+printf("Good Evening",time=19);
+break;
+case 21:
+printf("Good Evening",time=20);
+break;
+case 22:
+printf("Good Evening",time=21);
+break;
+case 23:
+printf("Good Evening",time=22);
+break;
+case 24:
+printf("Good Evening",time=23);
+break;
+default:
+printf("Invalid input");
+}
+return 0;
+}
+
---
+++ Fast/question 7 cpp.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+    printf("  ------------  \n");
+    printf(" /    /\\_/\\       \\ \n");
+    printf("/  ^        ^  \\ \n");
+    printf("|  ( .    . )  |\n");
+    printf("|  (   ||   )  |\n");
+    printf("|   \\  __  /   |\n");
+    printf(" \\   ------   / \n");
+    printf("  \\ K24-0609 /  \n");
+    printf("   ----------    \n");    
+return 0;
+}
+
+
