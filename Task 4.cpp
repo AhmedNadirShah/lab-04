550992de22e34d4fb40df09b434fd75892312812
AhmedNadirShah <ahmednadirshah99@gmail.com>
2024-11-01 01:05:24 +0500

    Add files via upload

---
+++ Task 4.cpp
@@ -0,0 +1,72 @@
+#include<stdio.h>
+int main()
+{int num;
+printf("Enter your number and should be greather than zero ");
+scanf("%d",&num);
+switch(num)
+{
+case 1:
+	printf("One",num=1);
+	break;
+case 2:
+	printf("two",num=2);
+	break;
+case 3:
+	printf("three",num=3);
+	break;
+case 4:
+	printf("four",num=4);
+	break;
+case 5:
+	printf("five",num=5);
+	break;
+case 6:
+	printf("six",num=6);
+	break;	
+case 7:
+	printf("seven",num=7);
+	break;
+case 8:
+	printf("eight",num=8);
+	break;
+case 9:
+	printf("nine",num=9);
+	break;
+case 10:
+	printf("ten",num=10);
+	break;
+case 11:
+	printf("eleven",num=11);
+	break;
+case 12:
+	printf("twelve",num=12);
+	break;
+case 13:
+	printf("thirteen",num=13);
+	break;
+case 14:
+	printf("fourteen",num=14);
+	break;
+case 15:
+	printf("fifteen",num=15);
+	break;
+case 16:
+	printf("sixteen",num=16);
+	break;
+case 17:
+	printf("Seventeen",num=17);
+	break;
+case 18:
+	printf("eighteen",num=18);
+	break;
+case 19:
+	printf("nineteen",num=19);
+	break;
+case 20:
+	printf("Twenty",num=20);
+	break;
+default:
+	printf("The number is greater then twenty");
+}
+return 0;
+}
---
+++ Task 6.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    printf("  ------------  \n");
+    printf(" /    /\\_/\\       \\ \n");
+    printf("/  ^        ^  \\ \n");
+    printf("|  ( .    . )  |\n");
+    printf("|  (   ||   )  |\n");
+    printf("|   \\  __  /   |\n");
+    printf(" \\   ------   / \n");
+    printf("  \\ K24-0609 /  \n");
+    printf("   ----------    \n");    
+return 0;
+}
